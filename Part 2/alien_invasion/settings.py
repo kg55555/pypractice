1867f001b3d2a7174ea00d7b9e2fa22e9f1877ef
Kevin Guo <kg55555@users.noreply.github.com>
2020-05-29 17:33:05 -0700

    Friday May 29

--- Part 2/alien_invasion/settings.py
+++ Part 2/alien_invasion/settings.py
@@ -7,3 +7,12 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (135, 206, 235)
+        # Ship settings
+        self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 3.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (20, 180, 150)
+        self.bullets_allowed = 8
+
